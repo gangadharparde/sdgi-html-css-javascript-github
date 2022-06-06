68c6c18bb0beef79b06a9882c9df2795048892c9
Gangadhar Parde <gangadhar.parde@gmail.com>
2022-06-06 16:00:13 +0530

    Added Sample Git Commands

---
+++ Github Training/Git Sample Commands.bat
@@ -0,0 +1,27 @@
+Create a new repository on the command line
+echo "# scrap-project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/gangadharparde/scrap-project.git
+git branch -M main
+git push -u origin main
+
+
+---------------------------------------------------
+
+Step 0 : git status
+Step 1 : git clone https://github.com/gangadharparde/scrap-project.git
+Then Copy Paste all you project files....
+Step 2 : cd scrap-project
+Step 3 : git add .
+Step 4 : git commit -m "Updated my project"
+Step 5 : git push
+
+
+git branch -a
+git remote add origin https://github.com/gangadharparde/scrap-project.git
+git push -u origin main
