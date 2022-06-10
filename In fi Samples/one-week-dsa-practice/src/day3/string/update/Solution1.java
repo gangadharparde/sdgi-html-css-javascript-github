47e3211e8a3807f66f00bc0d515213dcb6f456ac
Gangadhar Parde <gangadhar.parde@gmail.com>
2022-06-10 16:03:31 +0530

    Added more solutions

--- In fi Samples/one-week-dsa-practice/src/day3/string/update/Solution1.java
+++ In fi Samples/one-week-dsa-practice/src/day3/string/update/Solution1.java
@@ -1,24 +1,22 @@
 package day3.string.update;
 
 import java.util.Scanner;
-
-public class Solution1 {
+public class Solution1
+{
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] nn = new int[n];
-		for (int i = 0; i < n; i++) {
-			nn[i] = sc.nextInt();
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		char a[]=s.toCharArray();
+		int count=0;
+		for(int i=0;i<a.length-1;i++)
+		{
+		    if(a[i]=='1'&&a[i+1]=='0')
+		    {
+		        count+=1;
+		        a[i+1]='1';
+		       
+		    }
 		}
-		int min_temp = Integer.MAX_VALUE;
-		for (int i = 0; i < n; i++) {
-			if (i + 1 < n) {
-				min_temp = Math.min(min_temp, nn[i] + nn[i + 1]);
-			}
-		}
-		min_temp *= 2;
-		System.out.println("Output:");
-		System.out.println(min_temp);
-		sc.close();
+		System.out.println(count);
 	}
 }
